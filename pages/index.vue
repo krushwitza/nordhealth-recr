a5e16d27afef423c5069bc7cadd1655e55eb2e21
Piotr Krusicki <piotr.krusicki@onet.pl>
2024-10-07 11:59:01 +0200

    Initial project structure

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const showPassword = ref(false);
+</script>
+
+<template>
+  <div>
+    <form action="">
+      <label for="email">
+        Email:
+      </label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        required
+      >
+      
+      <label for="password">
+        Password:
+      </label>
+
+      <input
+        :type="showPassword ? 'text' : 'password'"
+        id="password"
+        name="password"
+        required
+      >
+      <button
+        type="button"
+        @click="showPassword = !showPassword"
+      >
+        {{ showPassword ? '🙈' : '👁️' }}
+      </button>
+
+      <button type="submit">
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/succes.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Success!</h1>
+    <p>Thank you for signing up for our product.</p>
+    <a href="/">Go back to the homepage</a>
+  </div>
+</template>
+
+<style scoped></style>
