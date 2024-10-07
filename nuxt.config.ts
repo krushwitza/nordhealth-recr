a51cdfa323de42f372e6567f9327733040816c5f
Piotr Krusicki <piotr.krusicki@onet.pl>
2024-10-07 15:23:55 +0200

    Add nord components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  ssr: false,
   devtools: { enabled: true },
   modules: [
     '@nuxt/eslint',
     '@vee-validate/nuxt',
-  ]
+  ],
+  plugins: [
+    {
+      src: '~/plugins/nordcomponents.ts',
+      mode: 'client'
+    }
+  ],
+  vite: {
+    vue: {
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.includes('-')
+        }
+      }
+    }
+  }
 })
