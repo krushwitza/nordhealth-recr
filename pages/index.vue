777ed77d3e130df703205bdb8e6c06ebabd470a0
Piotr Krusicki <piotr.krusicki@onet.pl>
2024-10-07 15:38:49 +0200

    Use nord component/styles in index page

--- pages/index.vue
+++ pages/index.vue
@@ -19,71 +19,88 @@ const showPassword = ref<Boolean>(false);
 const onSubmit = handleSubmit(async (values) => {
   navigateTo('/success')
 })
-
 </script>
 
 <template>
   <div>
-    <form @submit.prevent="onSubmit">
-      <div>
-        <label for="email">
-          Email:
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          v-bind="emailAttrs"
-          v-model="email"
-          aria-describedby="email-error"
-          :aria-invalid="!!errors.email"
-        >
-        <span
-          v-if="errors.email"
-          id="email-error"
-          role="alert"
-          aria-live="polite"
-        >
-          {{ errors.email }}
-        </span>
-      </div>
-      
-      <div>
-        <label for="password">
-          Password:
-        </label>
+    <nord-header>
+      <h1 class="n-typescale-l">Nord / Vue Example</h1>
+    </nord-header>
+    <main class="">
+      <nord-card class>
+        <form @submit.prevent="onSubmit">
+          <nord-stack>
+            <!-- email -->
+            <nord-input
+              label="Email"
+              type="email"
+              id="email"
+              name="email"
+              v-bind="emailAttrs"
+              v-model="email"
+              aria-describedby="email-error"
+              :aria-invalid="!!errors.email"
+            >
+              <span
+                slot="error"
+                v-if="errors.password"
+                id="password-error"
+                role="alert"
+                aria-live="polite"
+              >
+                {{ errors.password }}
+              </span>
+            </nord-input>
+          
+            <!-- pass -->
+            <nord-input
+              label="Password"
+              :type="showPassword ? 'text' : 'password'"
+              id="password"
+              name="password"
+              v-bind="passwordAttrs"
+              v-model="password"
+              aria-describedby="password-error"
+              :aria-invalid="!!errors.password"
+            >
+              <nord-button
+                slot="end"
+                type="button"
+                @click="showPassword = !showPassword"
+                :aria-label="showPassword ? 'Hide password' : 'Show password'"
+              >
+                <nord-icon v-if="showPassword" size="s" name="interface-edit-off"></nord-icon>
+                <nord-icon v-else size="s" name="interface-edit-on"></nord-icon>
+              </nord-button>
 
-        <input
-          :type="showPassword ? 'text' : 'password'"
-          id="password"
-          name="password"
-          v-bind="passwordAttrs"
-          v-model="password"
-          aria-describedby="password-error"
-          :aria-invalid="!!errors.password"
-        >
-        <span
-          v-if="errors.password"
-          id="password-error"
-          role="alert"
-          aria-live="polite"
-        >
-          {{ errors.password }}
-        </span>
-      </div>
-      <button
-        type="button"
-        @click="showPassword = !showPassword"
-        :aria-label="showPassword ? 'Hide password' : 'Show password'"
-      >
-        {{ showPassword ? '🙈' : '👁️' }}
-      </button>
+              <span
+                v-if="errors.password"
+                slot="error"
+                id="password-error"
+                role="alert"
+                aria-live="polite"
+              >
+                {{ errors.password }}
+              </span>
+            </nord-input>
 
-      <button type="submit">
-        Submit
-      </button>
-    </form>
+            <!-- checkbox -->
+            <nord-checkbox
+              label="Receive occasional product updates and announcements"
+              value="Value"
+            ></nord-checkbox>
+
+            <!-- submit -->
+            <nord-button type="submit" variant="primary">
+              Submit
+            </nord-button>
+          </nord-stack>
+        </form>
+      </nord-card>
+    </main>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
