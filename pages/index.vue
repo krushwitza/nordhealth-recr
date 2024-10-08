efbc7f1032957ea878a721ad72c4cd9f1eba5f4f
Piotr Krusicki <piotr.krusicki@onet.pl>
2024-10-08 19:16:53 +0200

    Fix linter errors

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@ import { formSchema } from '@/utils/validation.schema';
 
 // UI state
 const isLoading = ref(false);
-const showPassword = ref<Boolean>(false);
+const showPassword = ref<boolean>(false);
 
 // Form
 const { handleSubmit, defineField, errors } = useForm({
@@ -17,7 +17,7 @@ const [email, emailAttrs] = defineField('email');
 const [password, passwordAttrs] = defineField('password');
 
 // Form submission handler
-const onSubmit = handleSubmit(async (values) => {
+const onSubmit = handleSubmit(async () => {
   isLoading.value = true;
   const result = await $fetch('/api/wait');
   isLoading.value = false;
@@ -35,20 +35,20 @@ const onSubmit = handleSubmit(async (values) => {
           <nord-stack>
             <!-- email -->
             <nord-input
+              v-model="email"
               label="Email"
               type="email"
               name="email"
               v-bind="emailAttrs"
-              v-model="email"
               aria-describedby="email-error"
               :aria-invalid="!!errors.email"
               :expand="true"
               required
             >
               <span
-                slot="error"
                 v-if="errors.email"
                 id="email-error"
+                slot="error"
                 role="alert"
                 aria-live="polite"
               >
@@ -58,12 +58,12 @@ const onSubmit = handleSubmit(async (values) => {
           
             <!-- pass -->
             <nord-input
+              id="password"
+              v-model="password"
               label="Password"
               :type="showPassword ? 'text' : 'password'"
-              id="password"
               name="password"
               v-bind="passwordAttrs"
-              v-model="password"
               aria-describedby="password-error"
               :aria-invalid="!!errors.password"
               required
@@ -72,17 +72,17 @@ const onSubmit = handleSubmit(async (values) => {
               <nord-button
                 slot="end"
                 type="button"
-                @click="showPassword = !showPassword"
                 :aria-label="showPassword ? 'Hide password' : 'Show password'"
+                @click="showPassword = !showPassword"
               >
-                <nord-icon v-if="showPassword" size="s" name="interface-edit-off"></nord-icon>
-                <nord-icon v-else size="s" name="interface-edit-on"></nord-icon>
+                <nord-icon v-if="showPassword" size="s" name="interface-edit-off"/>
+                <nord-icon v-else size="s" name="interface-edit-on"/>
               </nord-button>
 
               <span
                 v-if="errors.password"
-                slot="error"
                 id="password-error"
+                slot="error"
                 role="alert"
                 aria-live="polite"
               >
@@ -94,7 +94,7 @@ const onSubmit = handleSubmit(async (values) => {
             <nord-checkbox
               label="Receive occasional product updates and announcements"
               value="Value"
-            ></nord-checkbox>
+            />
 
             <!-- submit -->
             <nord-button
