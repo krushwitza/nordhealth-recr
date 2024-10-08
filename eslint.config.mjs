8a44801acd9ae4f852effeb2660257f4ec2cc285
Piotr Krusicki <piotr.krusicki@onet.pl>
2024-10-08 19:35:51 +0200

    Disable vue/no-deprecated-slot-attribute

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,10 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // your custom flat configs go here, for example:
-  // {
-  //   files: ['**/*.ts', '**/*.tsx'],
-  //   rules: {
-  //     'no-console': 'off' // allow console.log in TypeScript files
-  //   }
-  // },
-  // {
-  //   ...
-  // }
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/no-deprecated-slot-attribute': 'off'
+    }
+  }
 )
